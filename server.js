68e10175f66bab47c98656ecd98e1566f85fef9a
circlerow <hoangthanhhy02@gmail.com>
2023-06-10 12:27:54 +0700

    Khá là Ok

--- server.js
+++ server.js
@@ -7,141 +7,16 @@ import controller from "./src/controller/Controller.js";
 import configViewEngine from './src/config/viewEngine.js'
 
 compiler.init(option) //compiler.init dùng để khởi tạo compiler
-
 configViewEngine(app);
+
 app.use(bodyP.json())  //app.use(bodyP.json()) dùng để lấy dữ liệu từ form
 app.use("/codemirror-5.65.12", express.static("G:/Project-2-20222/codemirror-5.65.12")) //dùng để lấy các file trong thư mục codemirror-5.65.12   
-app.use("/login.css", express.static("G:/Project-2-20222/login.css")) //dùng để lấy các file trong thư mục css
-app.use("/controller.js", express.static("G:/Project-2-20222/controller.js")) //dùng để lấy các file trong thư mục css
-app.use("/login.js", express.static("G:/Project-2-20222/login.js")) //dùng để lấy các file trong thư mục css
-app.get("/", function (req, res) { //app.get dùng để lấy dữ liệu từ form
-    compiler.flush(function () { //compiler.flush dùng để xóa các file đã compile
-        console.log("deleted") //in ra màn hình console
-    })
-    res.sendFile("G:/Project-2-20222/src/view/login.ejs")
-})
+app.get("/", controller.reset)
 app.get("/example", controller.getExample)
 app.get("/detail/example/:id", controller.getDetailExample)
-app.post("/login", async (req, res) => {
-    console.log("check:", req.body)
-    const rowCount = await controller.logindata(req.body.username, req.body.password)
-    console.log("checkrow:", rowCount)
-    if (rowCount > 0) {
-        res.send({ status: "success" })
-    }
-    else {
-        res.send({ status: "fail" })
-    }
+app.post("/login", controller.loginDataCheck)
+app.get("/index", controller.compilePage)
+app.post("/compile", controller.compilerMachine)
 
-})
 
-app.get("/index", (req, res) => {
-    res.sendFile("G:/Project-2-20222/src/view/index.ejs")
-})
-
-app.post("/compile", async (req, res) => {
-    var code = req.body.code //lấy code từ form
-    var input = req.body.input //lấy input từ form
-    var lang = req.body.lang //lấy ngôn ngữ từ form
-    await controller.createdata(req);
-    try {
-
-        if (lang == "Cpp") {
-            if (!input) {//nếu không có input thì chạy hàm compiler.compileCPP
-                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } }; // (uses g++ command to compile )
-                compiler.compileCPP(envData, code, (data) => {
-                    if (data.output) {
-                        res.send(data);//gửi dữ liệu về cho client
-                        controller.savedata(data)
-                    }
-                    else {
-                        res.send({ output: "error" })//gửi dữ liệu về cho client
-                    }
-                });
-            }
-            else {//nếu có input thì chạy hàm compiler.compileCPPWithInput
-                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } }; // (uses g++ command to compile )
-                compiler.compileCPPWithInput(envData, code, input, async (data) => {
-                    if (data.output) {
-                        res.send(data);
-                        controller.savedata(data);
-                    }
-                    else {
-                        res.send({ output: "error" })
-                        controller.savedata({ output: "error" })
-                    }
-                });
-            }
-        }
-        else if (lang == "Java") {
-            if (!input) {
-                var envData = { OS: "windows" };
-                compiler.compileJava(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                        controller.savedata(data)
-                    }
-                    else {
-                        res.send({ output: "error" })
-                        controller.savedata({ output: "error" })
-
-                    }
-                })
-            }
-            else {
-                //if windows  
-                var envData = { OS: "windows" };
-                //else
-                compiler.compileJavaWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                        controller.savedata(data)
-
-                    }
-                    else {
-                        res.send({ output: "error" })
-                        controller.savedata({ output: "error" })
-
-                    }
-                })
-            }
-        }
-        else if (lang == "python") {
-            if (!input) {
-                var envData = { OS: "windows" };
-                compiler.compilePython(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                        controller.savedata(data)
-
-                    }
-                    else {
-                        res.send({ output: "error" })
-                        controller.savedata({ output: "error" })
-
-                    }
-                });
-            }
-            else {
-                var envData = { OS: "windows" };
-                compiler.compilePythonWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                        controller.savedata(data)
-                    }
-                    else {
-                        res.send({ output: "error" })
-                        controller.savedata({ output: "error" })
-
-                    }
-                });
-            }
-        }
-    }
-    catch (e) {//bắt lỗi
-        console.log("error")//console.log dùng để in ra màn hình console
-    }
-
-})
-
-app.listen(8000)//app.listen dùng để lắng nghe cổng 8000
+app.listen(8000)
--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,4 +1,5 @@
 import pool from "../config/connectDB.js";
+import compiler from 'compilex';
 
 
 let createdata = async (req, res) => {
@@ -41,6 +42,133 @@ let getDetailExample = async (req, res) => {
     return res.render('exampleDetail.ejs', { info: obj });
 }
 
+let compilerMachine = async (req, res) => {
+    var code = req.body.code //lấy code từ form
+    var input = req.body.input //lấy input từ form
+    var lang = req.body.lang //lấy ngôn ngữ từ form
+    await createdata(req);
+    try {
+        if (lang == "Cpp") {
+            if (!input) {//nếu không có input thì chạy hàm compiler.compileCPP
+                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } }; // (uses g++ command to compile )
+                compiler.compileCPP(envData, code, (data) => {
+                    if (data.output) {
+                        res.send(data);//gửi dữ liệu về cho client
+                        savedata(data)
+                    }
+                    else {
+                        res.send({ output: "error" })//gửi dữ liệu về cho client
+                        savedata({ output: "error" })
+                    }
+                });
+            }
+            else {//nếu có input thì chạy hàm compiler.compileCPPWithInput
+                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } }; // (uses g++ command to compile )
+                compiler.compileCPPWithInput(envData, code, input, async (data) => {
+                    if (data.output) {
+                        res.send(data);
+                        savedata(data);
+                    }
+                    else {
+                        res.send({ output: "error" })
+                        savedata({ output: "error" })
+                    }
+                });
+            }
+        }
+        else if (lang == "Java") {
+            if (!input) {
+                var envData = { OS: "windows" };
+                compiler.compileJava(envData, code, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                        savedata(data)
+                    }
+                    else {
+                        res.send({ output: "error" })
+                        savedata({ output: "error" })
+
+                    }
+                })
+            }
+            else {
+                //if windows  
+                var envData = { OS: "windows" };
+                //else
+                compiler.compileJavaWithInput(envData, code, input, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                        savedata(data)
+
+                    }
+                    else {
+                        res.send({ output: "error" })
+                        savedata({ output: "error" })
+
+                    }
+                })
+            }
+        }
+        else if (lang == "python") {
+            if (!input) {
+                console.log("check:", code, input, lang)
+                var envData = { OS: "windows" };
+                compiler.compilePython(envData, code, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                        savedata(data)
+
+                    }
+                    else {
+                        res.send({ output: "error" })
+                        savedata({ output: "error" })
+
+                    }
+                });
+            }
+            else {
+                var envData = { OS: "windows" };
+                compiler.compilePythonWithInput(envData, code, input, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                        savedata(data)
+                    }
+                    else {
+                        res.send({ output: "error" })
+                        savedata({ output: "error" })
+
+                    }
+                });
+            }
+        }
+    }
+    catch (e) {//bắt lỗi
+        console.log("error")//console.log dùng để in ra màn hình console
+    }
+}
+let compilePage = (req, res) => {
+    res.sendFile("G:/Project-2-20222/src/view/index.ejs")
+}
+let loginDataCheck = async (req, res) => {
+    console.log("check:", req.body)
+    const rowCount = await logindata(req.body.username, req.body.password)
+    console.log("checkrow:", rowCount)
+    if (rowCount > 0) {
+        res.send({ status: "success" })
+    }
+    else {
+        res.send({ status: "fail" })
+    }
+}
+let reset = async (req, res) => {
+    {
+        compiler.flush(function () {
+            console.log("deleted")
+        })
+        res.render("G:/Project-2-20222/src/view/login.ejs")
+    }
+}
 export default {
-    createdata, savedata, logindata, getExample, getDetailExample
+    createdata, savedata, logindata, getExample, getDetailExample,
+    compilerMachine, compilePage, loginDataCheck, reset
 };
